6926b640f264840be6d5ed072a1e3e75b4d0926a
Kseni <cesty_cat@mail.ru>
2025-03-19 20:25:30 +0300

    lab is done

--- task2.py
+++ task2.py
@@ -2,39 +2,14 @@ import cv2
 import time
 import numpy as np
 
-
-def fly_pic():
-
-    img1 = cv2.imread('ref-point.jpg')
-    img2 = cv2.imread('fly64.png')
-
-    height1, width1 = img1.shape[:2]
-    height2, width2 = img2.shape[:2]
-
-    new_height = max(height1, height2)
-    new_width = max(width1, width2)
-
-    result = np.zeros((new_height, new_width, 3), dtype=np.uint8)
-
-    y_offset1 = (new_height - height1) // 2
-    x_offset1 = (new_width - width1) // 2
-
-    y_offset2 = (new_height - height2) // 2
-    x_offset2 = (new_width - width2) // 2
-
-    result[y_offset1:y_offset1 + height1, x_offset1:x_offset1 + width1] = img1
-    result[y_offset2:y_offset2 + height2, x_offset2:x_offset2 + width2] = img2
-
-    cv2.imshow('Fly', result)
-
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
-
-
 def video_spot():
     cap = cv2.VideoCapture(0)
     img = cv2.imread('ref-point.jpg')
+    fly = cv2.imread('fly64.png', cv2.IMREAD_UNCHANGED)
+
     img = cv2.resize(img, (100, 100))
+    fly = cv2.resize(fly, (100, 100))
+
     img_color = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     down_points = (640, 480)
     i = 0
@@ -60,15 +35,22 @@ def video_spot():
             w, h = img_color.shape[1], img_color.shape[0]
 
 
-# метка будет выделяться только в правой половине
+# метка будет выделяться только в правой 
+
             if x + (w // 2) > frame_width // 2:
 
                 cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 255, 0), 2)
 
+                overlay = fly.copy()
+                roi = frame[y:y+h, x:x+w]
+                fly_bgra = cv2.split(overlay)
+                alpha = fly_bgra[3] / 255.0
+                
+                for c in range(0, 3):
+                    roi[:, :, c] = alpha * fly_bgra[c] + (1 - alpha) * roi[:, :, c]
+
                 if i % 5 == 0:
-                    a = x + (w // 2)
-                    b = y + (h // 2)
-                    print(a, b)
+                    print(x + w//2, y + h//2)
 
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -83,5 +65,5 @@ def video_spot():
 
 if __name__ == '__main__':
     video_spot()
-    fly_pic()
+    
 cv2.waitKey(0)
